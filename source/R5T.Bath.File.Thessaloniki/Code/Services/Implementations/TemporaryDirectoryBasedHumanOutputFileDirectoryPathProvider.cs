5c081118661ed7e60a7da336fff53b3215668f9c
MinexAutomation <minex.automation@gmail.com>
2020-01-19 17:44:37 -0800

    Added a temporary directory-based human output file directory path provider implementation.

---
+++ source/R5T.Bath.File.Thessaloniki/Code/Services/Implementations/TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using R5T.Bath.File.Default;
+using R5T.Thessaloniki;
+
+
+namespace R5T.Bath.File.Thessaloniki
+{
+    public class TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider : IHumanOutputFileDirectoryPathProvider
+    {
+        private ITemporaryDirectoryPathProvider TemporaryDirectoryPathProvider { get; }
+
+
+        public TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider(ITemporaryDirectoryPathProvider temporaryDirectoryPathProvider)
+        {
+            this.TemporaryDirectoryPathProvider = temporaryDirectoryPathProvider;
+        }
+
+        public string GetHumanOutputFileDirectoryPath()
+        {
+            var humanOutputFileDirectoryPath = this.TemporaryDirectoryPathProvider.GetTemporaryDirectoryPath();
+            return humanOutputFileDirectoryPath;
+        }
+    }
+}
