97dfabe443302a334cc0e37c0c7670e50b4f1713
MinexAutomation <minex.automation@gmail.com>
2020-01-30 00:47:02 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Bath.File.Thessaloniki/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bath.File.Default;
+using R5T.Dacia;
+using R5T.Thessaloniki;
+
+
+namespace R5T.Bath.File.Thessaloniki
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider"/> implementation of <see cref="IHumanOutputFileDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddTemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider(this IServiceCollection services,
+            ServiceAction<ITemporaryDirectoryPathProvider> addTemporaryDirectoryPathProvider)
+        {
+            services
+                .AddSingleton<IHumanOutputFileDirectoryPathProvider, TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider>()
+                .RunServiceAction(addTemporaryDirectoryPathProvider)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider"/> implementation of <see cref="IHumanOutputFileDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IHumanOutputFileDirectoryPathProvider> AddTemporaryDirectoryBasedHumanOutputFileDirectoryPathProviderAction(this IServiceCollection services,
+            ServiceAction<ITemporaryDirectoryPathProvider> addTemporaryDirectoryPathProvider)
+        {
+            var serviceAction = new ServiceAction<IHumanOutputFileDirectoryPathProvider>(() => services.AddTemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider(
+                addTemporaryDirectoryPathProvider));
+            return serviceAction;
+        }
+    }
+}
